46830129f15517dc4741fb35fc69e83a5252b197
Igor Henrique Constant <igorhenriqueconstant@gmail.com>
2025-07-07 15:54:28 -0300

    Desenvolvimento

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from './pages/login/login.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { HomeComponent } from './pages/home/home.component';
 import { CriarPostagemComponent } from './pages/criar-postagem/criar-postagem.component';
+import { ListarPostagensDashboardComponent } from './pages/listar-postagens-dashboard/listar-postagens-dashboard.component';
 import { AuthGuard } from './auth.guard';
 
 export const routes: Routes = [
@@ -11,5 +12,5 @@ export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
     { path: 'criar-postagem', component: CriarPostagemComponent, canActivate: [AuthGuard] },
-
+    { path: 'listar-postagens', component: ListarPostagensDashboardComponent, canActivate: [AuthGuard] },
 ];
